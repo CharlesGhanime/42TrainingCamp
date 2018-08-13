24c2614193bf2a697cc97a9d1e988d8bf6968911
CharlesGhanime <ghanime.charles@gmail.com>
2018-08-13 03:43:04 +0200

    First push

---
+++ PISCINES/J02/ft_print_alphabet.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void ft_print_alphabet(void);
+
+
---
+++ PISCINES/tableau.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+int a=97;
+
+    while (a<123)
+    {
+
+    putchar(a);
+    a++;
+
+    }
+
+return 0;
+
+}
